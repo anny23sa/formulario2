592e6f8d9796d52649eafaf0c3531f6153e2cb2e
Anny Stecca <anny23sa@gmail.com>
2023-09-24 18:11:02 -0700

    se crean los componentes

---
+++ src/components/Registro/Registro.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Alert from '../Alert/Alert'
+import Formulario from '../Formulario/Formulario'
+import SocialButton from '../SocialButton/SocialButton'
+
+const Registro = () => {
+  return (
+    <div>
+        <Alert />
+        <Formulario />
+        <SocialButton />
+    </div>
+  )
+}
+
+export default Registro
