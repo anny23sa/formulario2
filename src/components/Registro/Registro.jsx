0b8c910cdbfa421ba21ee15f54ab2f7a5f30f8ef
Anny Stecca <anny23sa@gmail.com>
2023-09-25 22:47:49 -0700

    se agregan los iconos con una clase

--- src/components/Registro/Registro.jsx
+++ src/components/Registro/Registro.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import Alert from '../Alert/Alert'
-import Formulario from '../Formulario/Formulario'
 import SocialButton from '../SocialButton/SocialButton'
+import Formulario from '../Formulario/Formulario'
+import { Alert } from 'react-bootstrap'
 
 
 const Registro = () => {
+
+
+
   return (
     <div>
-        <Alert />
-        <Formulario />
-        <SocialButton />
+      <h1>Registro</h1>
+      {/* Utiliza SocialButton y pasa el icono como prop */}
+      <SocialButton icon="facebook" />
+      <SocialButton icon="github" />
+      <SocialButton icon="linkedin" />
+      <Formulario />
+      <Alert />
     </div>
-  )
+  );
 }
 
-export default Registro
+export default Registro;
--- src/components/SocialButton/SocialButton.jsx
+++ src/components/SocialButton/SocialButton.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
 
-const SocialButton = () => {
+/*import React from 'react';
+
+function SocialButton(props) {
+  // Accede al icono mediante props.icon
+  const { icon } = props;
+
   return (
-    <div>SocialButton</div>
-  )
+    <button className={`social-button ${icon}`}>
+      <i className={`fa fa-${icon}`}></i>{icon}
+    </button>
+  );
 }
 
-export default SocialButton
+export default SocialButton;*/
+
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './SocialButton.css'
+
+const SocialButton = ({ icon }) => {
+  let iconElement;
+  
+
+  switch (icon) {
+    case 'facebook':
+      iconElement = <FontAwesomeIcon icon={faFacebook} className="icon-grande"/>;
+      break;
+    case 'github':
+      iconElement = <FontAwesomeIcon icon={faGithub} className="icon-grande"/>;
+      break;
+    case 'linkedin':
+      iconElement = <FontAwesomeIcon icon={faLinkedin} className="icon-grande"/>;
+      break;
+  
+    default:
+      iconElement = null;
+  }
+
+  return (
+    <button className={`social-button ${icon}`}>
+      {iconElement}
+    </button>
+  );
+}
+
+export default SocialButton;
