cc8d005a2e76bb7eb9035e3c5bb09584870cfa1b
Anny Stecca <anny23sa@gmail.com>
2023-09-27 19:37:46 -0700

    Alert props

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -60,22 +60,3 @@ const Formulario = () => {
 
 export default Formulario;
 
-/*return (
-   <div className="container">
-       <h2>Dime tu nombre</h2>
-       {error ? <p className="error">Hay un error</p>: null}
-       <form onSubmit={handleSubmit}>
-           <div className="w-50">
-               <label className="form-label">Agrega tu nombre</label>
-               <input className="form-control" 
-               type="text" 
-               value={name} 
-               onChange={ e => setName(e.target.value)} />
-           </div>
-           <button className="btn btn-primary mt-4 w-50">Registrarse</button>
-       </form>
-   </div>
- );
-}
-
-export default Formulario*/
